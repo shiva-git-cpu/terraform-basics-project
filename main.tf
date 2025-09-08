9c5b6f9047a1acae475b149bd1c2099559d6ab37
shiva-git-cpu <shivaramguguloth24@gmail.com>
2025-09-08 12:52:29 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,49 @@
+# Security group for EC2
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install nginx1 -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+              echo "<h1>Hello from Terraform EC2 🚀</h1>" > /usr/share/nginx/html/index.html
+              EOF
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
